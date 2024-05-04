9c2fc93259c0475367bb73004c5806e6793af593
Moniqchege <quichege33@gmail.com>
2024-05-04 12:10:49 +0300

    commit

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,9 +21,11 @@ import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -41,6 +43,30 @@ const AuthForm = ({ type }: { type: string }) => {
     // 2. Define a submit handler.
     const onSubmit = async (data: z.infer<typeof formSchema>) => {
       setIsLoading(true);
+
+      try {
+        // Sign up with Appwrite & create plaid token
+
+        if (type === 'sign-up') {
+          // const newUser = await signUp(data)
+
+          // setUser(newUser);
+        }
+
+        if(type === 'sign-in') {
+          // const response = await signIn({
+          //  email: data.email,
+          //  password: data.password
+          // })
+
+           // if(response) router.push('/')
+          }
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setIsLoading(false);
+        }
+      }
 
     }
   return (
@@ -91,7 +117,7 @@ const AuthForm = ({ type }: { type: string }) => {
                   <CustomInput control={form.control} name='address1' label="Address" placeholder='Enter your specific address' />
                   <div className="flex gap-4">
                     <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
-                    <CustomInput control={form.control} name='state' label="State" placeholder='Example: KY' />
+                    <CustomInput control={form.control} name='state' label="State" placeholder='Enter your state' />
                   </div>
                   <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 11101' />
                 </>
